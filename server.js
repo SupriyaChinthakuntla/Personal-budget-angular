53f46d0e7146f788c26881be488e3c46e9f854fc
Priya Reddy <saipriyareddy94@gmail.com>
2020-10-07 12:41:18 -0400

    Shifted the code to another file

--- server.js
+++ server.js
@@ -3,46 +3,46 @@ const app = express();
 const port = 3000;
 app.use('/', express.static('public'));
 
-const budget = 
-{
-    myBudget:[
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 250
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-    {
-        title: 'Movies',
-        budget: 50
-    },  
-    {
-        title: 'Travel Expenses',
-        budget: 300
-    }, 
-    {
-        title: 'Trip',
-        budget: 200
-    },
-    {
-        title: 'Repairs',
-        budget: 44
-    },
-    {
-        title: 'Shopping',
-        budget: 100
-    },
-    {
-        title: 'Medical Expenses',
-        budget: 150
-    },
-]};
+const budget = require('./budget.json')
+// {
+//     myBudget:[
+//     {
+//         title: 'Eat out',
+//         budget: 30
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 250
+//     },
+//     {
+//         title: 'Groceries',
+//         budget: 90
+//     },
+//     {
+//         title: 'Movies',
+//         budget: 50
+//     },  
+//     {
+//         title: 'Travel Expenses',
+//         budget: 300
+//     }, 
+//     {
+//         title: 'Trip',
+//         budget: 200
+//     },
+//     {
+//         title: 'Repairs',
+//         budget: 44
+//     },
+//     {
+//         title: 'Shopping',
+//         budget: 100
+//     },
+//     {
+//         title: 'Medical Expenses',
+//         budget: 150
+//     },
+// ]};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
