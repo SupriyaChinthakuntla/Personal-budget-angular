4c87fc6b46a10a7350b96e4cf5a4fd7022dc812b
Priya Reddy <saipriyareddy94@gmail.com>
2020-10-07 13:10:47 -0400

    Reading from json file

--- server.js
+++ server.js
@@ -3,53 +3,14 @@ const app = express();
 const port = 3000;
 app.use('/', express.static('public'));
 
-const budget = require('./budget.json')
-// {
-//     myBudget:[
-//     {
-//         title: 'Eat out',
-//         budget: 30
-//     },
-//     {
-//         title: 'Rent',
-//         budget: 250
-//     },
-//     {
-//         title: 'Groceries',
-//         budget: 90
-//     },
-//     {
-//         title: 'Movies',
-//         budget: 50
-//     },  
-//     {
-//         title: 'Travel Expenses',
-//         budget: 300
-//     }, 
-//     {
-//         title: 'Trip',
-//         budget: 200
-//     },
-//     {
-//         title: 'Repairs',
-//         budget: 44
-//     },
-//     {
-//         title: 'Shopping',
-//         budget: 100
-//     },
-//     {
-//         title: 'Medical Expenses',
-//         budget: 150
-//     },
-// ]};
+const data= require('./budget.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(data);
 });
 
 app.listen(port, () => {
